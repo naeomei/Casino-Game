ba8b0e0a17fe7ed345d8415244173269d2e879f4
xznosage2 <gzellner@student.neumont.edu>
2023-08-27 11:38:39 -0600

    card model update

--- SlotMachineGame/SlotMachineGame/Models/CardModel.cs
+++ SlotMachineGame/SlotMachineGame/Models/CardModel.cs
@@ -2,6 +2,19 @@
 {
     public class CardModel
     {
+        int Number { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public string Image { get; set; }
 
+        public CardModel() { }
+
+        public CardModel(int number, string name, string value, string image)
+        {
+            Number = number;
+            Name = name;
+            Value = value;
+            Image = image;
+        }
     }
 }
